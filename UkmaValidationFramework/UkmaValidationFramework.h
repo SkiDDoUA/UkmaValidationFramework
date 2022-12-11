047e4a119dd21e14b8b2d0d725ded8f84856056c
Anton <antonlena516@gmail.com>
2022-12-11 19:05:56 +0200

    Initial Commit

---
+++ UkmaValidationFramework/UkmaValidationFramework.h
@@ -0,0 +1,18 @@
+//
+//  UkmaValidationFramework.h
+//  UkmaValidationFramework
+//
+//  Created by Anton Kolesnikov on 11.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UkmaValidationFramework.
+FOUNDATION_EXPORT double UkmaValidationFrameworkVersionNumber;
+
+//! Project version string for UkmaValidationFramework.
+FOUNDATION_EXPORT const unsigned char UkmaValidationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UkmaValidationFramework/PublicHeader.h>
+
+
